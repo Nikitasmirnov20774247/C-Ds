df2478cd31d90001675df134a81dc6e29fee1528
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-07 14:09:13 +0300

    ds1

--- Ds1/Program.cs
+++ Ds1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+int min = a;
+
+if (a > max ) max = a;
+if (b > max ) max = b;
+if (a < min ) min = a;
+if (b < min ) min = b;
+
+Console.WriteLine("Результат:");
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+Console.Write("min = ");
+Console.WriteLine(min);
