b0a27a3008c6a25535efa82bb2f154dac6460ee0
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-07 16:58:55 +0300

    ds4

---
+++ Ds4/Program.cs
@@ -0,0 +1,20 @@
+﻿int i = 1;
+bool not = true;
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+    if (i % 2 == 0)
+        {
+            Console.Write(i + ", ");
+            not = false;
+        }
+        i++;
+}
+if (not)
+{
+    Console.WriteLine("Нет чётных чисел!");
+}
