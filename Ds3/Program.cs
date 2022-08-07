b6d18c922a20f6d8785fe9c53cc5c5e131ae0a89
Nikita <Nikitasmirnov2000@yandex.ru>
2022-08-07 15:56:18 +0300

    ds3

---
+++ Ds3/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите число: ");
+int i = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введенное число является четным?");
+if (i % 2 == 0)
+Console.WriteLine("Да");
+else Console.WriteLine("Нет");
